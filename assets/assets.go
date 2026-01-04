53e389a164da74a91398787c25a2b74f566c324e
Oliviero Taleghani <o.taleghani@talesign.com>
2026-01-04 14:43:41 +0100

    added: better logs, refactored generation
    Added new logs. Now you can set the level of verbosity (either 'info' or 'debug') for better understanding what's happening under the hood. Also refactored most of the generation to have a clearer build.

--- assets/assets.go
+++ assets/assets.go
@@ -16,9 +16,6 @@ import "embed"
 //
 //go:embed canvas.js
 //go:embed graph.js
-//go:embed layout.html
-//go:embed style.css
-//go:embed app.js
 //go:embed Inter-Regular.woff2
 //go:embed Inter-Bold.woff2
 //go:embed Lato-Regular.woff2
